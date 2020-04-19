60ecd791741217c661e922c99d192c8e009903e7
Osi Ejedawe <osi.ejedawe@hotmail.com>
2020-04-19 21:34:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
     from = ["demon_souls", "dark_souls", "bloodborne", "sekiro"]
-    from << "elden_ring"
+    from << "elden_ring!"
 end
 
 def add_element_to_start_of_array(array, element)
