d801b1e3a7b48a52d82b72c19a3e23d8f11e3050
Osi Ejedawe <osi.ejedawe@hotmail.com>
2020-04-19 21:33:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-    ["demon_souls", "dark_souls", "bloodborne", "sekiro"]
-    << "arrays!"
+    array = ["demon_souls", "dark_souls", "bloodborne", "sekiro"]
+    array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
